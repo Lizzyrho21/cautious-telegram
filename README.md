e456f0e71d488c8878b5f4f31dab5c5b745cc2c8
Lizzy Wiley <Lizzyrho@gmail.com>
2022-02-11 11:57:53 -0600

    added client and server folder

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # cautious-telegram
 Storybeats App!
+
+Hello! Thanks for joining me on this progress journey to building my Storybeats app.
+
+________
+Technologies:
+
+Client side application:
+**React**,
+**Tailwind.CSS**,
+**Bootstrap** 
+
+Server side applications:
+**NodeJs**
+**Express/Axios**
+
+Database applications:
+**Postgres**
+
+
+Deployment:
+**Docker**,
+**Heroku**
+
+_____
+
+Phase I: 
+
+  Initalize Setup:
+  * Creating Client Folder :
+     - React-app
+     - Tailwind.css
+     - Bootstrap 
+
+  * Creating Server Folder :
+
+    - Express Generator? 
+    - NodeJS
+    - Express
+    - Axios
+    - Nodemon
+
+
+
+Login page:
+- Utilize Spotify Authorization 
+
+
---
+++ server/app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var port = 3008; // port specified!
+
+var indexRouter = require('./routes/index'); // example routes!
+var usersRouter = require('./routes/users'); // example routes!
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// listen on specified port we have 1 param, and a function. we use the port param to set the server.
+app.listen(port, function(err){
+  if (err) console.log("Error in server setup")
+  console.log("Server listening on Port", port);
+})
+
+module.exports = app; // exprot the bad boi 
